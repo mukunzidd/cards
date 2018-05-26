e3731db76762e0ed6e8a69221bedf7bd4faa36cf
Dodo <dmukunzi@gmail.com>
2018-05-26 12:44:29 +0200

    Initial Commit

---
+++ lib/cards.ex
@@ -0,0 +1,36 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello
+      :world
+
+  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six"]
+    suits = ["Spades", "Clubs", "Heart", "Diamonds"]
+
+    for value <- values, suit <- suits do
+      "#{value} of #{suit}"
+    end
+
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+end
